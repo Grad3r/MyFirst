6932db0f0aec53e07f7f43d83bebd5e5950e83e0
Dima <grad3r@ex.ua>
2019-01-19 12:14:19 +0200

    socketChat

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const app = express();
 const path = require('path');
 const expBars = require('express-handlebars');
 
+
+
+
+
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
+//
+// app.get ('/chat', function (req, res) {
+//     res.sendFile(__dirname + '/main.html');
+// });
+//
+// io.on('connection', (socket) => {
+//     console.log('User connected');
+//
+//         socket.on('sendMessage',(msg) => {
+//         io.emit('getMessge', msg);
+//     });
+// });
+
+
+
+
 app.use(express.static(path.join(__dirname, 'public', 'views')));
 
 app.use(express.json());
@@ -38,7 +60,34 @@ app.post('/loguser', logUser);
 app.get('/delete/:id', delUser);
 
 
-app.listen(5000, () => {
-    console.log('Listen 5000....')
+// app.listen(3000, () => {
+//     console.log('Listen 3000....')
+// });
+
+
+
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+app.get('/chat', function (req, res) {
+    res.sendFile(__dirname + '/main.html');
 });
 
+
+io.on('connection', (socket) => {
+
+    console.log('USER CONNECTED')
+
+    socket.on('sendMessage', (msg) => {
+        io.emit('getMessge', msg);
+    });
+
+
+});
+
+
+http.listen(3000, () => {
+    console.log('listening on 3000');
+});
